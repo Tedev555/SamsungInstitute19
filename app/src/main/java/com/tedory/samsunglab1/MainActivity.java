c8c373318ef1e1e69453888d348ecc51dc45c95e
ted555 <thanong984@gmail.com>
2019-03-20 17:35:14 +0700

    Init commit 2

---
+++ app/src/main/java/com/tedory/samsunglab1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.tedory.samsunglab1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText userNameEdt;
+    EditText passwordEdt;
+    Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userNameEdt = findViewById(R.id.user_edt);
+    }
+}
