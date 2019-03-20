4a7f01f12e8a8d7f67c3fc9ae6fc4cae3ec9edea
ted555 <thanong984@gmail.com>
2019-03-20 19:48:01 +0700

    Lesson: LinearLayout and ScrollView

--- app/src/main/java/com/tedory/samsunglab1/MainActivity.java
+++ app/src/main/java/com/tedory/samsunglab1/MainActivity.java
@@ -14,11 +14,38 @@ public class MainActivity extends AppCompatActivity {
     EditText passwordEdt;
     Button loginBtn;
 
+    String userStr;
+    String passStr;
+    String user = "Samsung";
+    String pass = "123";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         userNameEdt = findViewById(R.id.user_edt);
+        passwordEdt = findViewById(R.id.pass_edt);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userStr = userNameEdt.getText().toString();
+                passStr = passwordEdt.getText().toString();
+
+                if (userStr.equals(user) & passStr.equals(pass)) {
+                    //True
+                    Toast.makeText(getApplicationContext(),
+                            "Login Successful!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    //False
+                    Toast.makeText(getApplicationContext(),
+                            "Login Failed!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
