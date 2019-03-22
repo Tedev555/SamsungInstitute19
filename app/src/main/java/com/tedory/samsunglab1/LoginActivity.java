96ff69c43bde9e591f2c5af1f2c49b8683eeeba5
ted555 <thanong984@gmail.com>
2019-03-22 17:09:30 +0700

    Lesson-21-030-19: Intent and parsing data between activities

--- app/src/main/java/com/tedory/samsunglab1/LoginActivity.java
+++ app/src/main/java/com/tedory/samsunglab1/LoginActivity.java
@@ -1,13 +1,55 @@
 package com.tedory.samsunglab1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText userNameEdt;
+    EditText passwordEdt;
+    Button loginBtn;
+
+    String userStr;
+    String passStr;
+    String user = "Samsung";
+    String pass = "123";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userNameEdt = findViewById(R.id.user_edt);
+        passwordEdt = findViewById(R.id.pass_edt);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userStr = userNameEdt.getText().toString();
+                passStr = passwordEdt.getText().toString();
+
+                if (userStr.equals(user) & passStr.equals(pass)) {
+
+                    Intent intent = new Intent(getApplicationContext(),
+                            MainActivity.class);
+                    intent.putExtra("USERNAME", userStr);
+                    startActivity(intent);
+
+                } else {
+                    //False
+                    Toast.makeText(getApplicationContext(),
+                            "Login Failed!",
+                            Toast.LENGTH_SHORT).show();
+                    userNameEdt.setText("");
+                    passwordEdt.setText("");
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/tedory/samsunglab1/MainActivity.java
+++ app/src/main/java/com/tedory/samsunglab1/MainActivity.java
@@ -7,45 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText userNameEdt;
-    EditText passwordEdt;
-    Button loginBtn;
-
-    String userStr;
-    String passStr;
-    String user = "Samsung";
-    String pass = "123";
+    TextView mainTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userNameEdt = findViewById(R.id.user_edt);
-        passwordEdt = findViewById(R.id.pass_edt);
-        loginBtn = findViewById(R.id.login_btn);
+        mainTv = findViewById(R.id.main_tv);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                userStr = userNameEdt.getText().toString();
-                passStr = passwordEdt.getText().toString();
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("USERNAME");
+        mainTv.setText("Hello, " + userName);
 
-                if (userStr.equals(user) & passStr.equals(pass)) {
-                    //True
-                    Toast.makeText(getApplicationContext(),
-                            "Login Successful!",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    //False
-                    Toast.makeText(getApplicationContext(),
-                            "Login Failed!",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 }
